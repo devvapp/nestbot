381d3804ca08d6b966f0961da85bcc68a861edde
devvapp <devvapp@gmail.com>
2017-02-27 11:25:59 -0500

    Update nestbot.js
    Adding changes to comments

--- nestbot.js
+++ nestbot.js
@@ -4,14 +4,15 @@
 // We assume you have:
 // * a Wit.ai bot setup (https://wit.ai/docs/quickstart)
 // * a Messenger Platform setup (https://developers.facebook.com/docs/messenger-platform/quickstart)
-// You need to `npm install` the following dependencies: body-parser, express, request.
+// You need to `npm install` the following dependencies: body-parser, express, request, hackernews-api, node-wit.
 //
-// 1. npm install body-parser express request
+// 1. npm install --save body-parser express request hackernews-api node-wit
 // 2. Download and install ngrok from https://ngrok.com/download
-// 3. ./ngrok http 8445
-// 4. WIT_TOKEN=your_access_token FB_APP_SECRET=your_app_secret FB_PAGE_TOKEN=your_page_token node examples/messenger.js
+// 3. ./ngrok http 3000
+// 4. node nodebot.js
 // 5. Subscribe your page to the Webhooks using verify_token and `https://<your_ngrok_io>/webhook` as callback URL.
 // 6. Talk to your bot on Messenger!
+// 7. Code was developed using wit.ai quickstart
 
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
@@ -21,7 +22,7 @@ const request = require('request');
 const config = require('./config');
 const util = require('util');
 const hnews = require('hackernews-api');
-var cache = require('memory-cache');
+const cache = require('memory-cache');
 
 let Wit = null;
 let log = null;
